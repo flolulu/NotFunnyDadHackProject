ce8a1e76dfffbec104e3f217f670a968a2f688e9
flolulu <35403882+flolulu@users.noreply.github.com>
2021-02-06 16:46:40 -0500

    Add files via upload

--- SendAJoke.py
+++ SendAJoke.py
@@ -2,18 +2,19 @@
 
 from plyer import notification
 import time
+import pyjokes
 
 
 def notifyMe(title, message):
     notification.notify(
         title = title,          # the header, maybe it will say "Joke Time!"
-        message = message,      # the joke
+        message = pyjokes.get_joke(),      # the joke
         app_icon = "C:\\Users\\oluch\\PycharmProjects\\NotFunnyDadHackProject\\laugh.ico",      # the little image that will pop up with the notification
-        timeout = 15,           # how long the notification shows up for, here it is for 10 seconds
+        timeout = 100,           # how long the notification shows up for, here it is for 10 seconds
     )
 
 
 if __name__ == '__main__':
     while  True:
-        notifyMe("Joke time!", "How does a penguin build a house? \n Igloos it together")
+        notifyMe("Joke time!", print(pyjokes.get_joke()))
         time.sleep(14400)        # to make the program run every 4 hours
